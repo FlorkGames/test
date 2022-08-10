629a9b4319171a75349ae5063441ad909d821692
FlorkGames <105853494+FlorkGames@users.noreply.github.com>
2022-08-10 13:40:54 -0300

    Delete assets/custom_events directory

--- assets/custom_events/camera dance.lua
+++
@@ -1,8 +0,0 @@
-function onUpdate(elapsed)
-    songPos = getSongPosition()
-   local currentBeat = (songPos/5000)*(curBpm/60)
-   local currentBeat2 = (songPos/200)*(curBpm/200)
-   setProperty('camFollowPos.x',getProperty('camFollowPos.x') + (math.sin(currentBeat2) * 0.2))
-   setProperty('camFollowPos.y',getProperty('camFollowPos.y') + (math.cos(currentBeat2) * 0.2))
-
-end
